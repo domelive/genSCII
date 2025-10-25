6d2f87865cf069baf32c4869c27bb02af637caa0
domelive <domenico.livera@gmail.com>
2025-10-26 00:23:38 +0200

    feat: add command line options to make custom configuration

--- main.c
+++ main.c
@@ -1,27 +1,62 @@
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
+#include <getopt.h>
 
-#include "Image/Image.h"
 #include "Generator/Generator.h"
 
+static struct option long_options[] = {
+    { "input",  required_argument, 0, 'i' },
+    { "output", required_argument, 0, 'o' },
+    { "chars",  required_argument, 0, 'c' },
+    { "aspect", required_argument, 0, 'a' },
+    { "help",   no_argument,       0, 'h' },
+    {0, 0, 0, 0}
+};
+
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: ./genSCII <img_path>");
-        exit(EXIT_FAILURE);
+    const char* input_path = NULL;
+    const char* output_path = NULL;
+    const char* char_set = DEFAULT_CONFIG.char_set;
+    float aspect = DEFAULT_CONFIG.terminal_aspect_ratio;
+
+    int opt;
+    int long_index = 0;
+    while ((opt = getopt_long(argc, argv, "i:o:c:a:h", long_options, &long_index)) != -1) {
+        switch (opt) {
+            case 'i':
+                input_path = optarg;
+                break;
+            case 'o':
+                output_path = optarg;
+                break;
+            case 'c':
+                char_set = optarg;
+                break;
+            case 'a':
+                aspect = atof(optarg);
+                break;
+            case 'h':
+                printf("Usage: %s --input FILE --output FILE [--chars SET] [--aspect RATIO]\n", argv[0]);
+                return 0;
+            default:
+                fprintf(stderr, "Try '%s -h' for help.\n", argv[0]);
+                return 1;
+        }
     }
 
-    const char* path = argv[1];
-
-    Image* img = Image_load(path);    
-    if (!img) {
-        exit(EXIT_FAILURE);
+    if (!input_path || !output_path) {
+        fprintf(stderr, "Error: --input and --output are required.\n");
+        return -1;
     }
 
-    Generator_generateASCII(img);
+    ASCIIGenConfig cfg = DEFAULT_CONFIG;
+    cfg.char_set = char_set;
+    cfg.terminal_aspect_ratio = aspect;
 
-    Image_free(img);
+
+    if (!Generator_generateACIIFromFile(input_path, output_path, &cfg)) {
+        fprintf(stderr, "Failed to generate ASCII art.\n");
+        return 1;
+    }
+    
     return 0;
 }
